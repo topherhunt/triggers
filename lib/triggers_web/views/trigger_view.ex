96b99e2a457adc649d142640feccef2d8a19512d
Topher Hunt <hunt.topher@gmail.com>
2020-11-30 10:11:24 +0100

    (WIP) Delete button, creation throttle

--- lib/triggers_web/views/trigger_view.ex
+++ lib/triggers_web/views/trigger_view.ex
@@ -8,4 +8,8 @@ defmodule TriggersWeb.TriggerView do
   def due_by_today?(instance) do
     H.date_lte?(H.to_date(instance.due_at), Date.utc_today())
   end
+
+  def too_many_new_triggers?(triggers) do
+    Enum.count(triggers, & H.date_gte?(H.to_date(&1.inserted_at), Date.utc_today())) >= 1
+  end
 end
